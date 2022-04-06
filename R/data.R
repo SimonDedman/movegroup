b571a0e053dc134599684067f7599886c85577d9
Simon Dedman <simondedman@gmail.com>
2022-04-06 00:15:39 -0700

    data uploaded & documented

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Data: Tracks of lemon sharks off Bimini, Bahamas
+#'
+#' 1 paragraph description
+#'
+#' @format A data frame with 1308 rows and 5 variables:
+#' \describe{
+#'   \item{Datetime}{POSIXct datetime, format YYYY-MM-DD HH:MM:SS}
+#'   \item{Shark}{Individual shark ID code}
+#'   \item{T.Ph}{Tidal phase}
+#'   \item{Lat}{Decimal latitudes}
+#'   \item{Lon}{Decimal longitudes}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name TracksCleaned
+#' @usage data(TracksCleaned)
+#' @author Simon Dedman, \email{simondedman@@gmail.com}
+#' @source \url{http://datras.ices.dk}
+"TracksCleaned"
