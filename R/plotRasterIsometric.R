328bebe894f18d2ee4874260cfb1386c661129c8
Simon <simondedman@gmail.com>
2023-10-13 04:28:42 -0700

    plotRasterIsometric and plotTracksByID added

---
+++ R/plotRasterIsometric.R
@@ -0,0 +1,18 @@
+# Isometric and flat simple raster image creator from basic data
+# Simon Dedman, simondedman:gmail.com, 2023-10-12
+
+plotRasterIsometric <- functions(
+  
+) {
+  # copy PlotTracksByID until creation of
+  sf_locs
+  
+  # terra: create raster from sf_locs
+  # based on nSquares
+  
+  # plot raster basic
+  
+  # plot raster isometric
+  
+  # save
+}
