43f6ce6151f53e857ff0e1850bf92bf3780243ac
vheim <vital.heim@gmail.com>
2026-07-01 18:48:20 +0200

    added new .onAttach() message model choice

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "We strongly recommend that prior to using this package, you familiarize", 
+    "yourself with the underlying concepts of dBBMMs and dBGB.\n",
+    "Based on the characteristics of your data, your data may be ",
+    "inadequate for your model choice.\n",
+    "Please read the package vignette and familiarise yourself with the ",
+    "different options before deciding to run a dBBMM or dBGB.\n",
+    "See the references listed in the GitHub README at ",
+    "https://github.com/SimonDedman/movegroup ",
+    "for further background on these methods and use ?movegroup for guidance.\n"
+  )
+}
